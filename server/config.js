0427b0af9609675fffbf7a8ee75c8d73a1fad519
sudoyum21 <ba.samson.lam@gmail.com>
2018-06-26 20:36:35 -0400

    new branch dev

--- server/config.js
+++ server/config.js
@@ -1,9 +1,11 @@
 module.exports = {
     db: {
-        host: '192.168.70.98',
+        host: 'localhost',
+        // host: '192.168.70.98',
         database: 'topnet',
         user: 'root',
-        password: 'gate$2db'
+        password: 'password'
+        // password: 'gate$2db'
     },
 
     loglevel: "debug",  // Possible values: trace,debug,info,warn,error
