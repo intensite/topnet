b82b85bbbe647f85a4a912f799edab881f1e7850
sudoyum21 <ba.samson.lam@gmail.com>
2018-06-30 08:39:33 -0400

    adding dist express static. fixing undefined bug in mat paginator length property

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     db: {
-        host: 'localhost',
-        // host: '192.168.70.98',
+        // host: 'localhost',
+        host: '192.168.70.98',
         database: 'topnet',
         user: 'root',
-        password: 'password'
-        // password: 'gate$2db'
+        // password: 'password'
+        password: 'gate$2db'
     },
 
     loglevel: "debug",  // Possible values: trace,debug,info,warn,error
